a826f4de4b909505fa54466dcca7fdfe207bcadb
Mauro <marganaraz@juslapampa.gob.ar>
2020-03-17 12:36:25 -0300

    *.sol

--- OpenBalthazar.API/Controllers/EtherscanController.cs
+++ OpenBalthazar.API/Controllers/EtherscanController.cs
@@ -77,7 +77,7 @@ namespace OpenBalthazar.API.Controllers
             {
                 foreach (int l in rule.Lines)
                 {
-                    errores.Add(new Error(l, rule.Error, rule.Name));
+                    errores.Add(new Error(address + ".sol", l, rule.Error, rule.Name));
                     //sb.AppendFormat("<li><a href='#' onclick='mark({1} - 1)'>Line {1} - {2}</a>: {3}.</li>", l, rule.Name, rule.Error);
                 }
 
@@ -88,14 +88,121 @@ namespace OpenBalthazar.API.Controllers
             return Ok(errores);
         }
 
+        [HttpGet("scan_all")]
+        public ActionResult ScanAll()
+        {
+            // Armo la ruta completa al archivo
+            string filePath = _hostingEnvironment.ContentRootPath + "/Files/Etherscan/verifiedcontractaddress.csv";
+
+            string[] allFile = System.IO.File.ReadAllLines(filePath);
+
+            List<Error> errores = new List<Error>();
+
+            foreach (string f in allFile)
+            {
+                string[] columns = f.Split(',');
+
+                string address = columns[1].Substring(1, columns[1].Length - 2);
+
+                try
+                {
+                    string smartContractPath = _hostingEnvironment.ContentRootPath + "/Files/Etherscan/" + address + ".sol";
+
+                    // Leo el contrato de disco
+                    string smartContract = System.IO.File.ReadAllText(smartContractPath);
+
+                    // Lo deserializo
+                    var result = JsonSerializer.Deserialize<List<EtherscanSmartContract>>(smartContract);
+
+                    // Lo analizo y retorno los resultados
+                    ILanguage language = LanguageFactory.GetInstance(_hostingEnvironment.ContentRootPath + "/bin/Debug/netcoreapp3.1/", "sol");
+
+                    language.Code = result[0].SourceCode;
+
+                    language.Scan();
+                    StringBuilder sb = new StringBuilder();
+
+
+                    // Por cada regla muestro los resultados
+                    foreach (ILanguageRule rule in language.Rules)
+                    {
+                        foreach (int l in rule.Lines)
+                        {
+                            errores.Add(new Error(address + ".sol", l, rule.Error, rule.Name));
+                            //sb.AppendFormat("<li><a href='#' onclick='mark({1} - 1)'>Line {1} - {2}</a>: {3}.</li>", l, rule.Name, rule.Error);
+                        }
+
+                        //if (rule.Lines.Count == 0) sb.AppendFormat("<li>No se encontraron coincidencias con el patron {0}</li>", rule.Name);
+                    }
+
+                    if (errores.Count == 3) break;
+                }
+                catch(Exception ex)
+                {
+                    errores.Add(new Error(address + ".sol", 0, "El archivo no se pudo parsear", "JSON"));
+                }
+            }
+
+            // y lo retorno
+            return Ok(errores);
+        }
+
+        [HttpGet("read_all")]
+        public ActionResult ReadAll()
+        {
+            // Armo la ruta completa al archivo
+            string filePath = _hostingEnvironment.ContentRootPath + "/Files/Etherscan/verifiedcontractaddress.csv";
+
+            // Si el contrato no existe en disco, primero voy a leerlo a Etherscan
+            
+            if (System.IO.File.Exists(filePath))
+            {
+                string[] allFile = System.IO.File.ReadAllLines(filePath);
+
+                // por cada archivo, verifdico si existe
+                foreach(string f in allFile)
+                {
+                    string[] columns = f.Split(',');
+
+                    string address = columns[1].Substring(1, columns[1].Length - 2);
+
+                    string smartContractPath = _hostingEnvironment.ContentRootPath + "/Files/Etherscan/" + address + ".sol";
+
+                    if (!System.IO.File.Exists(smartContractPath)) 
+                    {
+                        // Instancio el cliente de la API Etherscan
+                        EtherscanClient client = new EtherscanClient(_configuration.GetSection("Etherscan.ApiKey").Value, _configuration.GetSection("Etherscan.URL").Value, _configuration.GetSection("Proxy").Value);
+
+                        // Obtengo el resultado JSON
+                        List<EtherscanSmartContract> contratos = client.GetSmartContract(address);
+
+                        // Lo serializo
+                        var texto = JsonSerializer.Serialize(contratos);
+
+                        // Lo almaceno en disco
+                        System.IO.File.AppendAllText(smartContractPath, texto);
+                    }                
+                }
+            }
+            else
+            {
+                //Error
+            }
+
+            
+            return Ok();
+        }
+
         public class Error
         {
+            public string FileName { get; set; }
             public int Line { get; set; } = 0;
             public string ErrorMsg { get; set; } = string.Empty;
             public string Rule { get; set; } = string.Empty;
 
-            public Error(int line, string errorMsg, string rule)
+            public Error(string fileName, int line, string errorMsg, string rule)
             {
+                FileName = fileName;
                 Line = line;
                 ErrorMsg = errorMsg;
                 Rule = rule;
--- OpenBalthazar.API/Controllers/ScannerController.cs
+++ OpenBalthazar.API/Controllers/ScannerController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Etherscan.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
@@ -77,6 +79,70 @@ namespace OpenBalthazar.API.Controllers
             }
 
             return Ok(language.Rules);
+        }
+
+        [HttpGet("GetCode")]
+        public ActionResult GetCode(string address)
+        {
+            // En base a la extension, levanto por REFLECTION el analizador correcto
+            //string ext = path.Substring(path.LastIndexOf(".") + 1, path.Length - 1 - path.LastIndexOf("."));
+
+
+            //if (!(ext.Equals("sol") || ext.Equals("vy")))
+            //{
+            //    ext += "sol";
+            //}
+
+            var lang = Request.HttpContext.Features.Get<IRequestCultureFeature>();
+            var dos = _httpContextAccessor.HttpContext.Request.Headers["Accept-Language"];
+            //Request.Headers[]
+
+            string idioma = "es";// Request.HttpContext.Features.Get<IRequestCultureFeature>().ToString(); //Request.UserLanguages[0];
+
+            Language idiom = Language.English;
+
+            if (idioma.ToLower().Contains("es"))
+            {
+                idiom = Language.Spanish;
+            }
+            else if (idioma.ToLower().Contains("en"))
+            {
+                idiom = Language.English;
+            }
+            else if (idioma.ToLower().Contains("pt"))
+            {
+                idiom = Language.Portugues;
+            }
+
+            ILanguage language = LanguageFactory.GetInstance(_hostingEnvironment.ContentRootPath + "/bin/Debug/netcoreapp3.1/", "sol");
+
+            string path = _hostingEnvironment.ContentRootPath + "/Files/Etherscan/" + address + ".sol";
+
+            string smartContract = System.IO.File.ReadAllText(path);
+
+            // Lo deserializo
+            var result = JsonSerializer.Deserialize<List<EtherscanSmartContract>>(smartContract);
+
+            language.Code = result[0].SourceCode;
+
+
+            language.Language = idiom;
+
+            //language.Scan();
+            //StringBuilder sb = new StringBuilder();
+
+            //// Por cada regla muestro los resultados
+            //foreach (ILanguageRule rule in language.Rules)
+            //{
+            //    foreach (int l in rule.Lines)
+            //    {
+            //        sb.AppendFormat("<li><a href='#' onclick='mark({1} - 1)'>Line {1} - {2}</a>: {3}.</li>", l, rule.Name, rule.Error);
+            //    }
+
+            //    if (rule.Lines.Count == 0) sb.AppendFormat("<li>No se encontraron coincidencias con el patron {0}</li>", rule.Name);
+            //}
+
+            return Ok(result[0].SourceCode);
         }
     }
 }
